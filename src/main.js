3abbe077dd8f35a565a1e20d6f9fc32cc21e563f
Лиза <liza@localhost.localdomain>
2020-11-21 21:55:35 +0300

    fix: отрисовка миникарточек без вложенных циклов

--- src/main.js
+++ src/main.js
@@ -8,12 +8,18 @@ import {createFilmCountTemplate} from "./view/film-count.js";
 import {createShowMoreBtnTemplate} from "./view/show-more-btn.js";
 import {createPopupTemplate} from "./view/popup.js";
 
+const FILM_CARD_COUNT = 5;
+const TOP_FILM_CARD_COUNT = 2;
+
 const render = (container, template, place) => {
   container.insertAdjacentHTML(place, template);
 };
 
-const FILM_CARD_COUNT = 5;
-const TOP_FILM_CARD_COUNT = 2;
+const addMiniCards = (list, count) => {
+  for(let i = 0; i < count; i++) {
+    render(list, createFilmCardTemplate(), `beforeend`);
+  }
+};
 
 const siteMainElement = document.querySelector(`.main`);
 const siteHeaderElement = document.querySelector(`.header`);
@@ -28,20 +34,15 @@ render(siteMainElement, createListTemplate(), `beforeend`);
 const siteFilmsSection = siteMainElement.querySelector(`.films-list`);
 const siteFilmsList = siteFilmsSection.querySelector('.films-list__container');
 
-for(let i = 0; i < FILM_CARD_COUNT; i++) {
-  render(siteFilmsList, createFilmCardTemplate(), `beforeend`);
-}
+addMiniCards(siteFilmsList, FILM_CARD_COUNT);
 
 render(siteFilmsSection, createShowMoreBtnTemplate(), `beforeend`);
 render(siteMainElement, createListTopTemplate(), `beforeend`);
 
-const siteTopFilms = siteMainElement.querySelectorAll('.films-list--extra .films-list__container');
+const [topRatedFilms, topCommentedFilms] = siteMainElement.querySelectorAll('.films-list--extra .films-list__container');
 
-for(let extraFilmSection of siteTopFilms) {
-  for(let i = 0; i < TOP_FILM_CARD_COUNT; i++) {
-    render(extraFilmSection, createFilmCardTemplate(), `beforeend`);
-  }
-}
+addMiniCards(topRatedFilms, TOP_FILM_CARD_COUNT);
+addMiniCards(topCommentedFilms, TOP_FILM_CARD_COUNT);
 
 render(siteFooterSection, createFilmCountTemplate(), `beforeend`);
 render(siteFooter, createPopupTemplate(), `afterend`);
