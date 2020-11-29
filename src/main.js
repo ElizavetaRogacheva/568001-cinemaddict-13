1b0cf3ab91f9af9612b7eab095ad29bea0001b39
Лиза <liza@localhost.localdomain>
2020-11-29 16:22:26 +0300

    создан класс меню, вынесены функции рендера в utils

--- src/main.js
+++ src/main.js
@@ -1,15 +1,15 @@
 import {createUserNameTemplate} from "./view/username.js";
-import {createMenuTemplate} from "./view/menu.js";
+import MenuView from "./view/menu.js";
 import {createSortTemplate} from "./view/sort.js";
 import {createListTemplate} from "./view/films-list.js";
 import {createFilmCardTemplate} from "./view/film-card.js";
 import {createListTopTemplate} from "./view/extra-films.js";
 import {createFilmCountTemplate} from "./view/film-count.js";
 import {createShowMoreBtnTemplate} from "./view/show-more-btn.js";
-import {createPopupTemplate} from "./view/popup.js";
 import {generateFilm} from "./mock/film.js";
 import {generateFilter} from "./mock/filter.js";
 import {generateUserName} from "./mock/username.js";
+import {renderTemplate, renderElement, RenderPosition} from "./utils.js";
 
 const FILM_CARD_COUNT = 18;
 const FILM_CARD_STEP = 5;
@@ -19,13 +19,10 @@ const films = new Array(FILM_CARD_COUNT).fill().map(generateFilm);
 const filters = generateFilter(films);
 const username = generateUserName(films);
 
-const render = (container, template, place) => {
-  container.insertAdjacentHTML(place, template);
-};
 
 const addMiniCards = (list, count) => {
   for(let i = 0; i < count; i++) {
-    render(list, createFilmCardTemplate(films[i]), `beforeend`);
+    renderTemplate(list, createFilmCardTemplate(films[i]), `beforeend`);
   }
 };
 
@@ -34,28 +31,28 @@ const siteHeaderElement = document.querySelector(`.header`);
 const siteFooter = document.querySelector(`.footer`);
 const siteFooterSection = siteFooter.querySelector(`.footer__statistics`);
 
-render(siteHeaderElement, createUserNameTemplate(username), `beforeend`);
-render(siteMainElement, createMenuTemplate(filters), `beforeend`);
-render(siteMainElement, createSortTemplate(), `beforeend`);
-render(siteMainElement, createListTemplate(), `beforeend`);
+renderTemplate(siteHeaderElement, createUserNameTemplate(username), `beforeend`);
+renderElement(siteMainElement, new MenuView().getElement(), RenderPosition.BEFOREEND);
+renderTemplate(siteMainElement, createSortTemplate(), `beforeend`);
+renderTemplate(siteMainElement, createListTemplate(), `beforeend`);
 
 const siteFilmsSection = siteMainElement.querySelector(`.films-list`);
 const siteFilmsList = siteFilmsSection.querySelector('.films-list__container');
 
 for (let i = 0; i < Math.min(films.length, FILM_CARD_STEP); i++) {
-  render(siteFilmsList, createFilmCardTemplate(films[i]), `beforeend`);
+  renderTemplate(siteFilmsList, createFilmCardTemplate(films[i]), `beforeend`);
 };
 
 if (films.length > FILM_CARD_STEP) {
   let renderFilmsCount = FILM_CARD_STEP;
-  render(siteFilmsSection, createShowMoreBtnTemplate(), `beforeend`);
+  renderTemplate(siteFilmsSection, createShowMoreBtnTemplate(), `beforeend`);
   const showMoreButton = siteFilmsSection.querySelector(`.films-list__show-more`);
 
   showMoreButton.addEventListener(`click`, (evt) => {
     evt.preventDefault();
     films
       .slice(renderFilmsCount, renderFilmsCount + FILM_CARD_STEP)
-      .forEach((film) => render(siteFilmsList, createFilmCardTemplate(film), `beforeend`));
+      .forEach((film) => renderTemplate(siteFilmsList, createFilmCardTemplate(film), `beforeend`));
 
     renderFilmsCount += FILM_CARD_STEP;
 
@@ -65,12 +62,11 @@ if (films.length > FILM_CARD_STEP) {
   });
 }
 
-render(siteMainElement, createListTopTemplate(), `beforeend`);
+renderTemplate(siteMainElement, createListTopTemplate(), `beforeend`);
 
 const [topRatedFilms, topCommentedFilms] = siteMainElement.querySelectorAll('.films-list--extra .films-list__container');
 
 addMiniCards(topRatedFilms, TOP_FILM_CARD_COUNT);
 addMiniCards(topCommentedFilms, TOP_FILM_CARD_COUNT);
 
-render(siteFooterSection, createFilmCountTemplate(), `beforeend`);
-render(siteFooter, createPopupTemplate(films[0]), `afterend`);
+renderTemplate(siteFooterSection, createFilmCountTemplate(), `beforeend`);
--- src/view/menu.js
+++ src/view/menu.js
@@ -1,12 +1,33 @@
-export const createMenuTemplate = (filters) => {
-  const [watchlist, history, favorites] = filters;
+import {createElement} from "./../utils.js";
+const createMenuTemplate = () => {
   return `<nav class="main-navigation">
     <div class="main-navigation__items">
       <a href="#all" class="main-navigation__item main-navigation__item--active">All movies</a>
-      <a href="#watchlist" class="main-navigation__item">Watchlist <span class="main-navigation__item-count">${watchlist.count}</span></a>
-      <a href="#history" class="main-navigation__item">History <span class="main-navigation__item-count">${history.count}</span></a>
-      <a href="#favorites" class="main-navigation__item">Favorites <span class="main-navigation__item-count">${favorites.count}</span></a>
+      <a href="#watchlist" class="main-navigation__item">Watchlist <span class="main-navigation__item-count">1</span></a>
+      <a href="#history" class="main-navigation__item">History <span class="main-navigation__item-count">1</span></a>
+      <a href="#favorites" class="main-navigation__item">Favorites <span class="main-navigation__item-count">1</span></a>
     </div>
     <a href="#stats" class="main-navigation__additional">Stats</a>
   </nav>`;
 };
+
+export default class Menu {
+  constructor() {
+    this._element = null;
+  }
+
+  getTemplate() {
+    return createMenuTemplate();
+  }
+
+  getElement() {
+    if (!this._element) {
+      this._element = createElement(this.getTemplate());
+    }
+    return this._element;
+  }
+
+  removeElement() {
+    this._element = null;
+  }
+}
