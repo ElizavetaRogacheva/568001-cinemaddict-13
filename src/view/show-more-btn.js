17da27f41c36606c4166e92bb7072260946d9ea8
Лиза <liza@localhost.localdomain>
2020-12-13 20:54:07 +0300

    наследование простых компонентов

--- src/view/show-more-btn.js
+++ src/view/show-more-btn.js
@@ -1,26 +1,11 @@
-import {createElement} from "./../utils.js";
+import AbstractView from "./abstract.js";
 
 const createShowMoreBtnTemplate = () => {
   return `<button class="films-list__show-more">Show more</button>`;
 };
 
-export default class ShowMoreBtn {
-  constructor() {
-    this._element = null;
-  }
-
+export default class ShowMoreBtn extends AbstractView {
   getTemplate() {
     return createShowMoreBtnTemplate();
-  }
-
-  getElement() {
-    if (!this._element) {
-      this._element = createElement(this.getTemplate());
-    }
-    return this._element;
-  }
-
-  removeElement() {
-    this._element = null;
   }
 }
